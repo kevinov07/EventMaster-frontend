91af02bba74b23f74176d817c590a19bcbe84cbb
AlejandroMesa111 <alejandro.mesa@correounivalle.edu.co>
2024-05-15 17:11:48 -0500

    Update: Login - Sign Up Terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventsComponent } from './components/events/events.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signUp', component: SignUpComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'events' , component: EventsComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 
