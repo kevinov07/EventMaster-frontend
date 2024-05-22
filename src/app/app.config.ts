836be713dd18e45edbdbd7e366feb4a639dfe4ad
Kevin Ordoñez <kevin.2000villegas@gmail.com>
2024-05-21 22:23:43 -0500

    chore: add toastr to use notifications

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideProtractorTestingSupport } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideProtractorTestingSupport(), provideRouter(routes), provideHttpClient()]
+  providers: [provideProtractorTestingSupport(), provideRouter(routes),
+    provideHttpClient(), provideAnimations(), provideToastr(
+      { timeOut: 3000, positionClass: 'toast-top-right', preventDuplicates: true }
+    ),]
 };
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { User } from '../../interfaces/user';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,17 +14,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  userService: UserService = inject(UserService);
-  router: Router = inject(Router);
+  signUpForm: FormGroup;
 
-  signUpForm = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20) ]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
-    confirmPassword: new FormControl('', [Validators.required])
-  });
-
-  constructor() {}
+  constructor(private userService: UserService, private router: Router, private toastr: ToastrService) {
+    this.signUpForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20) ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    });
+  }
 
   signupValidator(field: string) {
     const control = this.signUpForm.get(field);
@@ -33,7 +33,10 @@ export class SignUpComponent {
 
   signUp() {
     if (this.signUpForm.value.password !== this.signUpForm.value.confirmPassword) {
-      alert('Passwords do not match');
+      this.toastr.error('Passwords do not match.');
+      return;
+    } else if (this.signUpForm.invalid) {
+      this.toastr.error('Please fill out all fields correctly.');
       return;
     }
 
@@ -47,11 +50,12 @@ export class SignUpComponent {
       .subscribe({
         next: (response) => {
           console.log('Sign up successful:', response)
+          this.toastr.success('Sign up successful. Please log in.', 'Success!');
           this.router.navigate(['/login']);
         },
         error: (e) => {
           console.error('Error signing up:', e); 
-          alert('An error occurred while signing up. Please try again.')
+          this.toastr.error('An error occurred while signing up. Please try again.', 'Error');
         },
         complete: () => console.log('Sign up complete')
     });
