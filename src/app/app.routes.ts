068f2f81673f26530d4692ff2e2c5cc4c0368256
Kevin Ordoñez <kevin.2000villegas@gmail.com>
2024-05-25 14:38:16 -0500

    feat: Add create event route to events component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EventsComponent } from './components/events/events.component';
+import { CreateEventComponent} from './components/create-event/create-event.component';
 
 import { AuthGuard } from './utils/auth.guard';
 
@@ -10,7 +11,8 @@ export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signUp', component: SignUpComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-  {path: 'events' , component: EventsComponent},
+  {path: 'events', component: EventsComponent, canActivate: [AuthGuard]},
+  {path: 'events/create', component: CreateEventComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 
