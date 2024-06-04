10e642b924449cebf50d0778e2d56daefca8f0ea
AlejandroMesa111 <alejandro.mesa@correounivalle.edu.co>
2024-06-04 15:53:16 -0500

    Update: Header

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,HeaderComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
